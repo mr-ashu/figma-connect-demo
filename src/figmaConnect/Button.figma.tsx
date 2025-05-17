84ce34c94883780c1188a90f5be6747260504689
mr-ashu <ashutosh15th@gmail.com>
2025-05-17 14:35:33 +0530

    Initial setup with Code Connect

---
+++ src/figmaConnect/Button.figma.tsx
@@ -0,0 +1,5 @@
+import { figma } from '@figma/code-connect';
+import { Button } from '../components/Button';
+ 
+
+figma.connect(Button, 'https://www.figma.com/design/vSw0yf1BCInUS3AjH82yFi/Search--Opportunities-?node-id=5-5053&m=dev');
